456a30a6fe7863c92c73230200f632da9d43b3fe
Test account <Test account@DESKTOP-FHVU0OP>
2022-12-05 23:14:22 +0530

    frist commit

---
+++ Project/src/StringBuffer.java
@@ -0,0 +1,15 @@
+public class StringBuffer 
+{ 
+	public int length() {
+int length =90;
+return length;
+	}
+	public static void main(String args[]){
+{
+    StringBuffer  sb = new StringBuffer();
+    int len =sb.length();
+    System . out .println("length of a StringBuffer  object is:" + len);
+}
+	}
+	
+	}
---
+++ Project/src/StringBufferLength.java
@@ -0,0 +1,14 @@
+
+public class StringBufferLength{
+	
+public static void main(String[] args)
+{
+
+	int length()
+	{
+    StringBuffer  sb=new StringBuffer("Hellow world");
+    int len=sb.length;
+    System.out.println("length of a StringBuffer  object is:" + len);
+		
+	}
+}
