d1a67ffd8f5d275d9c5e2d0e84f7ca95cec66bc8
Test account <Test account@DESKTOP-FHVU0OP>
2022-12-06 16:32:03 +0530

    fisrt commit

---
+++ Project/src/lang/Datatype.java
@@ -0,0 +1,17 @@
+package lang;
+
+public class Datatype {
+public static void main(String args[])
+{
+	float  f=(float) 20.00;
+	long l=10;
+	System.out.println("value of l is "+l);
+	
+	
+	int x=20,y=30;
+	System.out.println(++x);
+	System.out.println(++y);
+	System.out.println(--y);
+			
+}
+}
--- Project/src/lang/Identifier.java
+++ Project/src/lang/Identifier.java
@@ -1,9 +1,25 @@
 package lang;
-
-public final class Identifier {
-
-	public static void main(String[] args) {
+class Identifier {
+	public int a=56;
+public void test()
+	{
+		System.out.println(+ a);
+	}
+	 private double b=5,c=8;
+private  void run()
+{
+		System.out.println(b+c );
+}
+float d=1.3f;
+  void disp() {
+	System.out.println(" display d the float value"+d);
+}
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Identifier ID =new Identifier();
+ID.test();
+ID.run();
+ID.disp();
 
 	}
 
