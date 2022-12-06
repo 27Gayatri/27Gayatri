98da69ab7976f60285aa26e443217bafa316ebf3
Test account <Test account@DESKTOP-FHVU0OP>
2022-12-06 16:44:20 +0530

    7commit

---
+++ Project/src/lang/Condition.java
@@ -0,0 +1,18 @@
+package lang;
+
+public class Condition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int y=20,x=30;
+if(y==47)
+{
+	System.out.println("value of y is 20");
+}
+else
+{
+	System.out.println("value x is ");
+
+}
+}
+}
